59ca493b79dc0d3a03ab14e1b5ce50137dcb6d92
irshad <irshadbhoonee4250@gmail.com>
2022-07-30 08:07:38 +0400

    Removing get

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
-const session = require("express-session")
-const MongoStore = require("connect-mongo")
-const { connectToMongo, cookieMiddleware } = require("./utils/utils")
+const { connectToMongo } = require("./utils/utils")
 
 const port = process.env.PORT || 5000
 
@@ -22,26 +20,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use("/public", express.static("public"))
 app.use(express.json({ limit: "10mb" }))
 app.use(cookieParser())
-
-if (process.env.NODE_ENV == "production") {
-  app.use(
-    session({
-      secret: "lmaolmao",
-      resave: false,
-      saveUninitialized: true,
-      cookie: { httpOnly: true, maxAge: 60 * 60 * 1000 },
-      store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI,
-        autoRemove: "interval",
-        autoRemoveInterval: 10,
-      }),
-    })
-  )
-  app.use(cookieMiddleware)
-}
-
-app.set("view engine", "ejs")
-app.engine("ejs", require("ejs").__express)
 
 const rootRouter = require("./routes/root")
 const searchRouter = require("./routes/search")
--- routes/root.js
+++ routes/root.js
@@ -6,100 +6,6 @@ const Userlog = require("../schemas/userlog")
 
 const Message = require("../schemas/message")
 
-router.get("/", async (req, res) => {
-  res.render("home")
-})
-
-router.get("/home", (req, res) => {
-  res.redirect("/")
-})
-
-router.get("/news", (req, res) => {
-  res.render("news")
-})
-
-router.get("/pastPapers", (req, res) => {
-  res.render("pastPapers")
-})
-
-router.get("/pastPapers/papers", async (req, res) => {
-  const { subject, year } = req.query
-
-  if (!subject || !year) {
-  }
-
-  const papers = await getPapers(subject, year)
-  if (papers.length === 0) {
-    res.render("404")
-    return
-  }
-
-  const finalResults = papers.map((result) => {
-    let object = {}
-    object.subject = result.subject
-
-    let month
-    let prefix = result.year.slice(0, 1)
-
-    if (prefix == "m") {
-      month = "March"
-    } else if (prefix == "s") {
-      month = "June"
-    } else if (prefix == "w") {
-      month = "Nov"
-    }
-
-    let title = month + " " + result.yearInt.toString() + " P" + result.variant
-    object.title = title
-
-    const link =
-      "https://papers.gceguide.com/A%20Levels/" +
-      result.subject +
-      "/" +
-      result.yearInt +
-      "/"
-
-    const qpLink = "/view/" + result.pdfname
-    const msLink = "/view/" + result.pdfname + "?type=ms"
-
-    const rawQpLink = link + result.pdfname
-    const rawMsLink = link + result.pdfname.replace("qp", "ms")
-
-    object.qpLink = qpLink
-    object.msLink = msLink
-    object.rawQpLink = rawQpLink
-    object.rawMsLink = rawMsLink
-
-    return object
-  })
-
-  res.render("papers", { results: finalResults, subject: subject, year: year })
-
-  function getPapers(subject, year) {
-    return new Promise(async (resolve, reject) => {
-      const results = await getQpCollection()
-        .find(
-          {
-            $and: [{ subject: subject }, { yearInt: parseInt(year) }],
-          },
-          {
-            projection: {
-              subject: 1,
-              yearInt: 1,
-              year: 1,
-              variant: 1,
-              pdfname: 1,
-            },
-          }
-        )
-        .sort({ variant: 1 })
-        .toArray()
-
-      resolve(results)
-    })
-  }
-})
-
 router.post("/getPapers", async (req, res) => {
   const { subject, year } = req.body
 
@@ -176,45 +82,6 @@ router.post("/getPapers", async (req, res) => {
         .toArray()
 
       resolve(results)
-    })
-  }
-})
-
-router.get("/about", (req, res) => {
-  res.render("about")
-})
-
-router.get("/view/:pdfName", async (req, res) => {
-  const pdfName = req.params.pdfName
-  const type = req.query.type
-
-  const pdfObject = await getPdfObject()
-
-  if (!pdfObject || pdfObject.length === 0) {
-    res.status(404).render("404")
-    return
-  }
-
-  const pdf = pdfObject[0]
-  const grade = pdf.grade ? pdf.grade : "A"
-  const pdfLink = `https://papers.gceguide.com/${grade} Levels/${pdf.subject}/${
-    pdf.yearInt
-  }/${type == "ms" ? pdf.pdfname.replace("qp", "ms") : pdf.pdfname}`
-
-  res.render("viewpdf", { pdfUrl: pdfLink })
-
-  function getPdfObject() {
-    return new Promise((resolve, reject) => {
-      getQpCollection()
-        .find({ pdfname: { $eq: pdfName } })
-        .project({ body: 0 })
-        .toArray((err, result) => {
-          if (err) {
-            resolve(null)
-          } else {
-            resolve(result)
-          }
-        })
     })
   }
 })
@@ -340,9 +207,5 @@ function validateEmail(input) {
     return false
   }
 }
-
-router.get("/contact", (req, res) => {
-  res.render("contact")
-})
 
 module.exports = router
